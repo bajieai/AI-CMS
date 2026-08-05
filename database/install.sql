1269c228ad14213edd1fd1607d1d579ef43b92bc
八界AI <admin@bajieai.com>
2026-08-05 13:09:13 +0800

    邀请码修复SQL合并到install.sql并删除独立文件

--- database/install.sql
+++ database/install.sql
@@ -5345,3 +5345,6 @@ CREATE TABLE `{prefix}webhook_log` (
 SET FOREIGN_KEY_CHECKS = 1;
 SET UNIQUE_CHECKS = 1;
 COMMIT;
+
+-- V2.9.42: 修复 member 表空邀请码唯一索引冲突
+UPDATE i8j_member SET invite_code = UPPER(SUBSTR(MD5(CONCAT(id, username, UNIX_TIMESTAMP())), 1, 8)) WHERE invite_code = '' OR invite_code IS NULL;
